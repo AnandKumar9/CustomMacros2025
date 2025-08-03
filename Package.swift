3848a7043cf0baaa1fbb4864a9bd2d45c4b91c10
Anand Kumar <write2anand.kumar@gmail.com>
2025-08-03 11:01:26 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "CustomMacros2025",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CustomMacros2025",
+            targets: ["CustomMacros2025"]
+        ),
+        .executable(
+            name: "CustomMacros2025Client",
+            targets: ["CustomMacros2025Client"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "CustomMacros2025Macros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(name: "CustomMacros2025", dependencies: ["CustomMacros2025Macros"]),
+
+        // A client of the library, which is able to use the macro in its own code.
+        .executableTarget(name: "CustomMacros2025Client", dependencies: ["CustomMacros2025"]),
+
+    ]
+)
